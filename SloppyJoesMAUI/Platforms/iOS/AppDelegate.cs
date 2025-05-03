77cc087aa04f680fd23d7c91604c064d45bb185c
chajus1 <chajus1@googlemail.com>
2025-05-03 21:49:58 +0100

    Add project files.

---
+++ SloppyJoesMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SloppyJoesMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
